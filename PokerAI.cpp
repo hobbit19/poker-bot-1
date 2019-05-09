ca08faac4099590df6fe75a1a8d7aaefc810c87f
Austin Born <austinborn212@gmail.com>
2019-05-08 20:00:38 -0500

    Added first file

---
+++ PokerAI.cpp
@@ -0,0 +1,21 @@
+/*
+Just an inkling of an idea that would be really cool to implement over break:
+Build an AI program that plays poker.
+*/
+
+/*
+Input information:
+ - Your hand
+ - Hand equity table
+ - Win ch
+ - Betting round
+ - Your $$
+ - Opponent's $$
+ - Previous bet information
+
+Decision:
+ - Call
+ - Raise (+ Raise $$)
+ - Fold
+*/
+
